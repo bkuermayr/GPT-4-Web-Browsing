2d82ed7211c544edeef7c3dd1b8d84a70f71e25e
bkuermayr <benjamin@kuermayr.com>
2024-07-11 18:38:22 +0200

    result backend thread safe

--- src/app.py
+++ src/app.py
@@ -24,7 +24,8 @@ def make_celery(app):
     celery = Celery(
         app.import_name,
         broker=app.config['broker_url'],
-        backend=app.config['result_backend']
+        backend=app.config['result_backend'],
+        result_backend_thread_safe=True
     )
     celery.conf.update(app.config)
     return celery
