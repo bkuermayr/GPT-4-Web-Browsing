e0a3e81da291a969f02edab77577cc987121c8f4
bkuermayr <benjamin@kuermayr.com>
2024-07-12 10:52:39 +0200

    remove some more pritns

--- src/llm_answer.py
+++ src/llm_answer.py
@@ -85,13 +85,13 @@ if __name__ == "__main__":
 
     # Retrieve relevant documents using embeddings
     retriever = EmbeddingRetriever()
-    print("Generating embeddings: " + serper_response['links'] + "\n" + web_contents)
+
     try:
         relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
     except Exception as e:
         print("Exception while retrieving embeddings: ", e)
     formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
-    print(formatted_relevant_docs)
+    # print(formatted_relevant_docs)
 
     # Measure the time taken to get an answer from the GPT model
     start = time.time()
