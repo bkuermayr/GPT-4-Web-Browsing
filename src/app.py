1dbacf8af400f65588e368c889c5e2057ec055aa
bkuermayr <benjamin@kuermayr.com>
2024-07-13 21:55:37 +0200

    change max redis connections to 4

--- src/app.py
+++ src/app.py
@@ -43,7 +43,7 @@ def make_celery(app):
 
     # Configure Celery to use the same Redis connection pool
     celery.conf.broker_transport_options = {
-        'max_connections': 10,
+        'max_connections': 4,
     }
 
     return celery
@@ -52,66 +52,65 @@ celery = make_celery(app)
 
 @celery.task(name='app.process_query_task')
 def process_query_task(data):
-    with redis.StrictRedis(connection_pool=redis_pool, decode_responses=True) as redis_conn:
-        query = data.get('search_query', '')
-        prompt = data.get('prompt', '')
-        output_format = data.get('output_format', "")
-        profile = data.get('profile', "")
-        search_location = data.get('search_location', "")
-        search_language = data.get('search_language', "")
-        output_language = data.get('output_language', "")
-        job_id = data.get('job_id', "")
-        product_id = data.get('product_id', "")
-        use_web_search = data.get('use_web_search', True)
+    query = data.get('search_query', '')
+    prompt = data.get('prompt', '')
+    output_format = data.get('output_format', "")
+    profile = data.get('profile', "")
+    search_location = data.get('search_location', "")
+    search_language = data.get('search_language', "")
+    output_language = data.get('output_language', "")
+    job_id = data.get('job_id', "")
+    product_id = data.get('product_id', "")
+    use_web_search = data.get('use_web_search', True)
 
-        logging.info(f'Received query: {query}, search_location: {search_location}, search_language: {search_language}, output_language: {output_language}')
-        logging.info(f'Received prompt: {prompt}')
-        content_processor = GPTAnswer()
+    logging.info(f'Received query: {query}, search_location: {search_location}, search_language: {search_language}, output_language: {output_language}')
+    logging.info(f'Received prompt: {prompt}')
+    content_processor = GPTAnswer()
 
-        if use_web_search:
-            web_contents_fetcher = WebContentFetcher(query=query, search_location=search_location, search_language=search_language, output_language=output_language)
-            web_contents, serper_response = web_contents_fetcher.fetch()
-            retriever = EmbeddingRetriever()
-            relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
-            formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
+    if use_web_search:
+        web_contents_fetcher = WebContentFetcher(query=query, search_location=search_location, search_language=search_language, output_language=output_language)
+        web_contents, serper_response = web_contents_fetcher.fetch()
+        retriever = EmbeddingRetriever()
+        relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
+        formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
 
-            # if no urls are found, return json reponse with empty answer
-            if not formatted_relevant_docs:
-                response = {
-                    'query': query,
-                    'job_id': job_id,
-                    'product_id': product_id,
-                    'answer': {},
-                    'gpt_answer_time': 0,
-                    'output_language': output_language,
-                    'reference_cards': [],
-                }
-                return response
-        else:
-            formatted_relevant_docs = None
-            serper_response = None
+        # if no urls are found, return json reponse with empty answer
+        if not formatted_relevant_docs:
+            response = {
+                'query': query,
+                'job_id': job_id,
+                'product_id': product_id,
+                'answer': {},
+                'gpt_answer_time': 0,
+                'output_language': output_language,
+                'reference_cards': [],
+            }
+            return response
+    else:
+        formatted_relevant_docs = None
+        serper_response = None
 
-        start = time.time()
-        ai_message_obj = content_processor.get_answer(prompt, formatted_relevant_docs, output_language, output_format, profile)
-        answer = ai_message_obj.content + '\n'
-        end = time.time()
+    start = time.time()
+    ai_message_obj = content_processor.get_answer(prompt, formatted_relevant_docs, output_language, output_format, profile)
+    answer = ai_message_obj.content + '\n'
+    end = time.time()
 
-        logging.info(f'Generated answer in {end - start} seconds')
+    logging.info(f'Generated answer in {end - start} seconds')
 
-        locator = ReferenceLocator(answer, serper_response)
-        reference_cards = locator.locate_source()
+    locator = ReferenceLocator(answer, serper_response)
+    reference_cards = locator.locate_source()
 
-        response = {
-            'query': query,
-            'job_id': job_id,
-            'product_id': product_id,
-            'answer': answer,
-            'gpt_answer_time': end - start,
-            'output_language': output_language,
-            'reference_cards': reference_cards,
-        }
+    response = {
+        'query': query,
+        'job_id': job_id,
+        'product_id': product_id,
+        'answer': answer,
+        'gpt_answer_time': end - start,
+        'output_language': output_language,
+        'reference_cards': reference_cards,
+    }
 
-        return response
+    return response
     
 @app.route('/api/query', methods=['POST'])
 def process_query():
