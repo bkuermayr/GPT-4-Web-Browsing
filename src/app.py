43dff599196d0ee7ce040938457386f2125ecc19
bkuermayr <benjamin@kuermayr.com>
2024-07-13 22:44:11 +0200

    fix python rq setup

--- src/app.py
+++ src/app.py
@@ -1,14 +1,8 @@
 from flask import Flask, request, jsonify
-import time
-import json
-import logging
-from fetch_web_content import WebContentFetcher
-from retrieval import EmbeddingRetriever
-from llm_answer import GPTAnswer
-from locate_reference import ReferenceLocator
 from rq import Queue
 from rq.job import Job
 from worker import conn
+from tasks import process_query_task
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -20,7 +14,7 @@ q = Queue(connection=conn)
 def process_query():
     data = request.get_json()
     job = q.enqueue_call(
-        func=process_query_task, args=(data,), result_ttl=5000
+        func='tasks.process_query_task', args=(data,), result_ttl=5000
     )
     return jsonify({"task_id": job.get_id()})
 
@@ -47,69 +41,6 @@ def task_status(task_id):
 @app.route('/')
 def hello():
     return 'Hello, World!'
-
-def process_query_task(data):
-    query = data.get('search_query', '')
-    prompt = data.get('prompt', '')
-    output_format = data.get('output_format', "")
-    profile = data.get('profile', "")
-    search_location = data.get('search_location', "")
-    search_language = data.get('search_language', "")
-    output_language = data.get('output_language', "")
-    job_id = data.get('job_id', "")
-    product_id = data.get('product_id', "")
-    use_web_search = data.get('use_web_search', True)
-
-    logging.info(f'Received query: {query}, search_location: {search_location}, search_language: {search_language}, output_language: {output_language}')
-    logging.info(f'Received prompt: {prompt}')
-    content_processor = GPTAnswer()
-
-    if use_web_search:
-        web_contents_fetcher = WebContentFetcher(query=query, search_location=search_location, search_language=search_language, output_language=output_language)
-        web_contents, serper_response = web_contents_fetcher.fetch()
-        retriever = EmbeddingRetriever()
-        relevant_docs_list = retriever.retrieve_embeddings(web_contents, serper_response['links'], query)
-        formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
-
-        # if no urls are found, return json response with empty answer
-        if not formatted_relevant_docs:
-            response = {
-                'query': query,
-                'job_id': job_id,
-                'product_id': product_id,
-                'answer': {},
-                'gpt_answer_time': 0,
-                'output_language': output_language,
-                'reference_cards': [],
-            }
-            return response
-    else:
-        formatted_relevant_docs = None
-        serper_response = None
-
-    start = time.time()
-    ai_message_obj = content_processor.get_answer(prompt, formatted_relevant_docs, output_language, output_format, profile)
-    answer = ai_message_obj.content + '\n'
-    end = time.time()
-
-    logging.info(f'Generated answer in {end - start} seconds')
-
-    locator = ReferenceLocator(answer, serper_response)
-    reference_cards = locator.locate_source()
-    # Parse the JSON string to a Python dictionary (JSON object)
-    answer_json_object = json.loads(answer)
-
-    response = {
-        'query': query,
-        'job_id': job_id,
-        'product_id': product_id,
-        'answer': answer_json_object,
-        'gpt_answer_time': end - start,
-        'output_language': output_language,
-        'reference_cards': reference_cards,
-    }
-
-    return response
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', threaded=False)
