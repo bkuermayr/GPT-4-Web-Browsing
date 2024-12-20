182bc69430c403de186af7a91de73535d5c9de37
Danis Katadzic <e12314182@student.tuwien.ac.at>
2024-12-20 23:56:35 +0100

    URL und Attribute auch im Prompt enthalten

--- src/DatabaseUtil.py
+++ src/DatabaseUtil.py
@@ -4,6 +4,8 @@ from supabase import create_client
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 client = create_client(url,key)
+assetUrl = 'https://oarreivvqvbvowbekecs.supabase.co/storage/v1/object/public/assets/'
+
 
 def findValueCustomFields(customFields, name):
     output = ''
@@ -13,32 +15,20 @@ def findValueCustomFields(customFields, name):
                 output = f'{output} {y}'
     return output
 
+def getURLLink(product_id):
+    assets = client.table('product_assets_with_assets').select('*').eq('product_id',product_id).order('order',desc=False).execute().data
+    for x in assets:
+        if 'image' in x['type']:
+            return f'{assetUrl}{x["path"]}'
+    return None
+
 if __name__ == '__main__':
     try:
         products_ids = ['4248240']
         response = client.table('automation').select('*').eq("id",6).single().execute()
         automationFields = client.table('automation_field').select('special_field, is_search_term').eq("automation_id",6).execute().data
-        products = client.table('products').select('id,title').in_("id",products_ids).execute().data
-        for x in automationFields:
-            print(x['special_field'])
         autoData = response.data
-        searchAttributes = []
-        aiAttributes = []
-        for x in automationFields:
-            if 'category' in x['special_field'].lower() or 'categories' in x['special_field'].lower():
-                continue
-            if x['is_search_term']:
-                searchAttributes.append(x['special_field'])
-            else:
-                aiAttributes.append(x['special_field'])
-        autoData['searchAttributes'] = searchAttributes
-        autoData['aiAttributes'] = aiAttributes 
         products = client.table('products').select('id,title,custom_fields').in_("id",products_ids).filter("parent_id","is","null").execute().data
-        searchAttributes.append('Website Größe')
-        searchAttributes.append('Farbe DE')
-        searchVal = ''
-        #for i in searchAttributes:
             #searchVal = f'{searchVal} {findValueCustomFields(products.get("custom_fields"),i)}'
-        print(products)
     except Exception as e:
         print(e)
--- src/tasks.py
+++ src/tasks.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 import time
 import ssl
-from DatabaseUtil import findValueCustomFields, client
+from DatabaseUtil import findValueCustomFields, client, getURLLink
 from celery.signals import task_postrun
 
 import json
@@ -63,7 +63,7 @@ def process_query_task(productData,automationData):
         searchVal = f'{searchVal} {temp}'
     for j in aiAttr:
         temp = findValueCustomFields(customFields,j)
-        aiVal = f'{aiVal} {temp}'
+        aiVal =f'{aiVal} {j}: {temp} \n'
     query = query + searchVal
     #url = data.get('whitelist',"")
     #temp = ""
@@ -99,6 +99,7 @@ def process_query_task(productData,automationData):
         '''f = open(f"demofile{product_id}.txt", "w")
         f.write(f'{relevant_docs_list.__str__()}')
         f.close()'''
+        assetUrl = getURLLink(product_id)
         formatted_relevant_docs = content_processor._format_reference(relevant_docs_list, serper_response['links'])
         if not formatted_relevant_docs:
             response = {
@@ -117,7 +118,7 @@ def process_query_task(productData,automationData):
         formatted_relevant_docs = None
         serper_response = None
     start = time.time()
-    ai_message_obj = content_processor.get_answer(prompt, formatted_relevant_docs, output_language, output_format, profile)
+    ai_message_obj = content_processor.get_answer(prompt, formatted_relevant_docs, output_language, output_format, profile,assetUrl,aiVal)
     answer = ai_message_obj.content
     answer = clean_json_string(answer)
     end = time.time()
