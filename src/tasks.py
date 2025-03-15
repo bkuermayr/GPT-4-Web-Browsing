e83d687e4ccdf0ff1499ec83a13de66887d877c1
Danis Katadzic <e12314182@student.tuwien.ac.at>
2025-03-15 13:44:54 +0100

    Changed the updatedStatus for categorization, to better differentiate between them two

--- src/tasks.py
+++ src/tasks.py
@@ -99,7 +99,7 @@ def task_postrun_notifier_category(state=None, retval=None, task_id=None, args=N
             client.table('automation_job_data').insert({'product_id':product_id,'automation_job_id':aID,'success':success,'data':data}).execute()
     else:
         client.table('automation_job_data').insert({'product_id':product_id,'automation_job_id':aID,'success':False,'data':{'error':retval.__str__()},'error':retval.__str__()}).execute()
-    client.rpc("increment_processed_products", {'job_id': aID}).execute()
+    client.rpc("increment_processed_products", {'job_id': aID, "updatedstatus": "completed categorization"}).execute()
 
 @celery.task
 def process_query_task(productData,automationData):
