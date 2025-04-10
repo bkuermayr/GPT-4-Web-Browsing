ea918f046e2c616eae0b5e191c123676d908e998
Danis Katadzic <e12314182@student.tuwien.ac.at>
2025-04-10 10:55:55 +0200

    Fixed Key Error

--- src/tasks.py
+++ src/tasks.py
@@ -93,7 +93,7 @@ def task_postrun_notifier_extraction_variant(state=None, retval=None, task_id=No
                 inserts.append({'product_id':i,'automation_job_id':aID,'success':False,'data':data, 'error': data})
             client.table('automation_job_data').insert(inserts).execute()
         else:
-            data = retval['answer']['answer']
+            data = retval['answer']
             print(data)
             for i in variants_ids:
                 help = {
